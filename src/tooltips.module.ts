09d18375f167130865fa08b141be3fdae738a7ea
Brian Soumakian <bsoumakian@gmail.com>
2017-09-03 17:17:08 -0700

    remove forRoot and forChild

--- src/tooltips.module.ts
+++ src/tooltips.module.ts
@@ -2,7 +2,6 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 import { Tooltip } from './tooltip.directive';
 import { TooltipBox } from './tooltip-box.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   entryComponents: [
@@ -20,17 +19,4 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ]
 })
 export class TooltipsModule {
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: TooltipsModule,
-      providers: [IonicModule, BrowserAnimationsModule]
-    };
-  }
-
-  static forChild(): ModuleWithProviders {
-    return {
-      ngModule: TooltipsModule,
-      providers: [IonicModule]
-    };
-  }
 }
